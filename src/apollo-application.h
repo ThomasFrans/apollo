e339f0d5a499514b63ba2c3d05ceac8b5b637843
Thomas Frans <franske2000@gmail.com>
2021-07-01 13:05:48 +0200

    initial commit

---
+++ src/apollo-application.h
@@ -0,0 +1,9 @@
+#include <gtkmm/application.h>
+#include <gtkmm/applicationwindow.h>
+
+namespace Apollo
+{
+    class Application : public Gtk::Application 
+    {
+    };
+} // namespace Apollo
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+#include "apollo-application.h"
+
+int main(int argc, char* argv[])
+{
+    Glib::RefPtr<Gtk::Application> app = Apollo::Application::create("org.thomas.apollo");
+    
+    return app->make_window_and_run<Gtk::ApplicationWindow>(argc, argv);
+}
